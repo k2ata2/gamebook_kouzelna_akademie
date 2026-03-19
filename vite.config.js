e7bcedf17e3462a68367b9e8615bef8203d4755f
Kateřina Chromčáková <zziirraaffkkaa@gmail.com>
2026-03-19 22:21:49 +0100

    jou

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/gamebook_kouzelna_akademie/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
