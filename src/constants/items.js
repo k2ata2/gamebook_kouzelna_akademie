ff0697085301f0fbfb28af03598bb49557ffb1ae
Kateřina Chromčáková <zziirraaffkkaa@gmail.com>
2026-03-19 22:16:02 +0100

    built web

--- src/constants/items.js
+++
@@ -1,3 +0,0 @@
-export const consumables = ['Svačina', 'Lektvar zdraví', 'Lektvar bublin', 'Zářivá houba'];
-
-export const usableItems = ['Svačina', 'Lektvar zdraví', 'Lektvar bublin', 'Zářivá houba'];
--- src/lib/loadFonts.js
+++
@@ -1,9 +0,0 @@
-const FONT_LINK_ID = 'magic-academy-fonts';
-
-if (typeof document !== 'undefined' && !document.getElementById(FONT_LINK_ID)) {
-  const fontsLink = document.createElement('link');
-  fontsLink.id = FONT_LINK_ID;
-  fontsLink.href = 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400&display=swap';
-  fontsLink.rel = 'stylesheet';
-  document.head.appendChild(fontsLink);
-}
